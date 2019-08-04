8ac8895590e6db0d1d247698b5522da25bd73750
Marley <marleyxiong0@gmail.com>
2019-08-04 14:12:30 -0700

    positioning

--- car.js
+++ car.js
@@ -1,20 +1,36 @@
 const CAR_WIDTH = 25;
 const CAR_LENGTH = 50;
 
+const POS_DICT = {
+  'N': [ WIDTH/2 - DOT_WIDTH/2 - ((ROAD_WIDTH - DOT_WIDTH)/2 - CAR_WIDTH)/2 - CAR_WIDTH, get_y(HEIGHT)],
+  'S': [ WIDTH/2 + DOT_WIDTH/2 +((ROAD_WIDTH - DOT_WIDTH)/2 - CAR_WIDTH)/2, get_y(CAR_LENGTH)],
+  'E': [0, get_y(HEIGHT/2)]
+  // 'S':
+  // 'W': 
+}
+const SIZE_DICT = {
+  'E': [CAR_WIDTH, CAR_LENGTH],
+  'W': [CAR_WIDTH, CAR_LENGTH],
+  'N': [CAR_LENGTH, CAR_WIDTH],
+  'S': [CAR_LENGTH, CAR_WIDTH],
+}
 class Car {
-  constructor(x, y, dir, speed) {
-    this.xpos = x;
-    this.ypos = get_y(y);
-    //this.direction = dir;
-    this.direction = [1,0];
+  constructor(pos, dir, speed) {
+    this.xpos = POS_DICT[pos][0];
+    this.ypos = POS_DICT[pos][1];
+    this.direction = dir;
     //this.ypos = random(height);
     this.speed = speed;
     this.color = color(255,180,0);
+    this.width = SIZE_DICT[pos][0];
+    this.length = SIZE_DICT[pos][1];
+
   }
   
   drive() {
     this.xpos = this.xpos % WIDTH;
     this.xpos += this.speed * this.direction[0];
+    this.ypos += this.speed * this.direction[1];
   }
  
   // // brake method
@@ -32,6 +48,6 @@ class Car {
     // body of the car
     fill(this.color);
     rectMode(CORNER);
-    rect(this.xpos, this.ypos, CAR_LENGTH, CAR_WIDTH);
+    rect(this.xpos, this.ypos, this.length, this.width);
   }
 }
--- sketch.js
+++ sketch.js
@@ -1,7 +1,8 @@
 let myCar;
 function setup() {
   createCanvas(WIDTH, HEIGHT);
-  myCar = new Car(50, 397, [0,1], 4);
+//  myCar = new Car(50, 397, [0,1], 4);
+	myCar = new Car('N', [0,0], 4);
 }
 
 function draw() {
