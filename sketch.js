68ce8c03054051d8c4f0d09609366e27aed0a97b
Marley <marleyxiong0@gmail.com>
2019-08-04 14:33:44 -0700

    array

--- sketch.js
+++ sketch.js
@@ -1,15 +1,23 @@
 let myCar;
+let arr = [];
+dirs = ['N', 'E', 'S', 'W'];
 function setup() {
   createCanvas(WIDTH, HEIGHT);
 //  myCar = new Car(50, 397, [0,1], 4);
 	eCar = new Car('E', 4);
+	for (let i=0; i < 10; i++){
+		var car = new Car(dirs[i % 4],4);
+		arr.push(car);
+	}
 	// wCar = new Car('W', 4);
 }
 
 function draw() {
   drawRoads();
-  eCar.drive();
-  eCar.display();
+  for (let i=0; i < 10; i++){
+		arr[i].drive();
+		arr[i].display();
+	}
   // wCar.drive();
   // wCar.display();
 }
